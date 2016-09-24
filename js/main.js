49906563744474a7c145ebd3247127ce427a1f2f
Eric Wang <ezwang1@gmail.com>
2016-09-24 19:51:59 -0400

    update schedule

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
 /* $(function() {
-	window.setTimeout(unhide,500);
+    window.setTimeout(unhide,500);
 });
 function unhide() {
-	$('.shell-inv').removeClass('shell-inv')
+    $('.shell-inv').removeClass('shell-inv')
 }
 */
 $(document).ready(function(){
-	$('a[href*=#]').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var $target = $(this.hash);
-			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-			 if ($target.length) {
-			 	var targetOffset = $target.offset().top - 100;
-				$('html,body').animate({scrollTop: targetOffset}, 1000);
-				return false;
-			}
-		}
-	});
-
-	if($(window).width() > 500) {
-		$(".shell-inv").show(function() {
-			$(this).find("iframe").prop("src",function(){
-				return $(this).data("src");
-			});
-			setTimeout(function(){$(".shell-inv").removeClass("shell-inv");},1000);
-		});
-	}
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+             if ($target.length) {
+                var targetOffset = $target.offset().top - 100;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
 });
